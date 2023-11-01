f597230cf950a77fb4939b32bf90a5a8aa1c74d5
MateusJSouza <mateusjdev@gmail.com>
2023-11-01 11:34:43 -0300

    feat: :recycle: adding a variation of the ExternalLink component

--- src/app/components/ExternalLink.tsx
+++ src/app/components/ExternalLink.tsx
@@ -1,19 +1,33 @@
 import { faUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { ComponentProps } from 'react'
+import { ComponentProps, ReactNode } from 'react'
+import { cn } from '../utils/cn'
 
 type ExternalLinkProps = ComponentProps<'a'> & {
   text: string
+  icon?: ReactNode
+  variant?: 'iconLeft'
 }
 
-export function ExternalLink({ text, ...rest }: ExternalLinkProps) {
+export function ExternalLink({
+  text,
+  icon,
+  variant,
+  ...rest
+}: ExternalLinkProps) {
   return (
     <a
-      className="border-none bg-none text-brand-blue text-xs uppercase flex items-center gap-2 font-bold transition-colors border-b-[1px_solid_transparent] h-[19px] leading-[19px] hover:border-brand-blue"
+      className={cn(
+        'border-none bg-[none] text-brand-blue text-xs uppercase flex items-center gap-2 font-bold transition-colors h-[19px] leading-[19px]',
+        'border-b-transparent hover:border-solid border-brand-blue',
+        variant === 'iconLeft' && 'flex-row-reverse',
+      )}
       {...rest}
     >
       {text}
-      <FontAwesomeIcon icon={faUpRightFromSquare} className="w-3 h-3" />
+      {icon ?? (
+        <FontAwesomeIcon icon={faUpRightFromSquare} className="w-3 h-3" />
+      )}
     </a>
   )
 }
