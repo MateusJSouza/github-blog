d715417febe4940bf04bb804597ac8a02d548efe
MateusJSouza <mateusjdev@gmail.com>
2023-11-01 11:33:43 -0300

    feat: :sparkles: changing momentjs language

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client'
 import { App } from './App'
 import './index.css'
 
+import moment from 'moment'
+import 'moment/dist/locale/pt-br'
+
+moment.locale('pt-br')
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
